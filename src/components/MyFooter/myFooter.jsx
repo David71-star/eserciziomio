f4716a66c81bba2af1c281d0b94135733a6b3637
David71-star <davidcortes024@gmail.com>
2024-02-12 16:19:57 +0100

    Create myFooter.jsx

---
+++ src/components/MyFooter/myFooter.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Container from "react-bootstrap/esm/Container";
+
+
+
+const Down = () => {
+    return (
+        <>
+        <Container>
+        <div className="footer bg-danger">
+            info: number: 5648397573
+        </div>
+        </Container>
+      </>
+);
+};
+
+export default Down;
