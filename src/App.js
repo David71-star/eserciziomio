b0917451a40d9cbe06a3e6ac7e7aa05cf1be959c
David71-star <davidcortes024@gmail.com>
2024-02-15 18:06:11 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import Cards from "./components/AllTheBooks/AllTheBooks.jsx";
+// import AllTheBooks from "./components/AllTheBooks/AllTheBooks.jsx";
+import Singlecard from "./components/SingleBook/SingleBook.jsx";
 import Down from "./components/MyFooter/myFooter.jsx";
 import Navigation from "./components/MyNav/MyNav.jsx";
 import Welcome from "./components/Welcome/welcome.jsx";
+import BookSearch from "./components/AllTheBooks/AllTheBooks.jsx";
 
 function App() {
   return (
     <>
       <Navigation />
       <Welcome />
-      <Cards />
+      <Singlecard />
+      {/* <BookSearch books={books} /> */}
       <Down />
     </>
   );
